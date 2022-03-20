ff84af218adec6da9c78eef764078aed333ea0b6
Максим Колтович <12345kolt@gmail.com>
2022-03-20 16:16:14 +0300

    Готово

--- tests/Header.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Header from '../src/views/Axios.vue'
-
-describe('Axios.vue', () => {
-  it('renders', () => {
-    const wrapper = mount(Header)
-    expect(wrapper.html())
-  })
-})
